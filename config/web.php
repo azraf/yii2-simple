452690466b5f83f93847750fb3b8872f3182587a
Azraf <azraf.git@makewebsmart.com>
2014-11-22 13:20:35 +0600

    Updated as yii2-admin recent update

--- config/web.php
+++ config/web.php
@@ -97,6 +97,18 @@ $config = [
                 ],
             ],
         ],
+        'i18n' => [
+            'translations' => [
+                        '*' => [
+                                'class' => 'yii\i18n\PhpMessageSource',
+                                'basePath' => '@app/messages', // if advanced application, set @frontend/messages
+                                'sourceLanguage' => 'en',
+                                'fileMap' => [
+                                        //'main' => 'main.php',
+                                ],
+                        ],
+                ],
+        ],
         'db' => require(__DIR__ . '/db.php'),
     ],
     'as access' => [
